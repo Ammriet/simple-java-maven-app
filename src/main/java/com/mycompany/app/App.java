863a2c1feac0f31c0fa3458922d4b4073a93443c
Ammriet <101466198+Ammriet@users.noreply.github.com>
2024-07-29 23:02:53 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,8 @@ package com.mycompany.app;
  */
 public class App
 {
-
-    private final String message = "Hello World!";
+   // Amrit Paudel
+    private final String message = "This is Jenkins demo";
 
     public App() {}
 
